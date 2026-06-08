159f2597e2bf9447da775879c1be988cbc6832b8
MuhdAbbasHamdard <muhdabbaskan@gmail.com>
2026-06-08 17:28:40 +0430

    use react hook from library for validation froms

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Form from "./components/Form";
+import SecondForm from "./components/SecondForm";
+import ThirdForm from "./components/ThirdForm";
 
 const App = () => {
   return (
     <div>
       {/* <Navbar /> */}
 
-      <Form />
+      {/* <Form /> */}
+      {/* <SecondForm /> */}
+      <ThirdForm></ThirdForm>
     </div>
   );
 };
